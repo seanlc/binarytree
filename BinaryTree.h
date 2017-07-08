57d56d18d6299311cbbf8552a7f1d5fdb8eafde9
Sean <seanlcarstensen@gmail.com>
2017-07-07 21:16:55 -0700

    added test simple harness for delete

--- BinaryTree.h
+++ BinaryTree.h
@@ -19,7 +19,7 @@ class BinaryTree
 {
   public:
     BinaryTree()
-    : root (nullptr)
+    : root (nullptr), numNodes (0)
     {}
     ~BinaryTree()
     {}
@@ -29,7 +29,10 @@ class BinaryTree
         return root == nullptr;
     }
     bool isFull();
-    int getLength();
+    int getLength()
+    {
+        return numNodes;
+    }
     string getItem(int key)
     {
 	tNode * temp = findNode(root,key);
@@ -39,8 +42,28 @@ class BinaryTree
     {
 	tNode * newNode = new tNode(key,val);
 	insert(root,newNode);
+	++numNodes;
     }
-    void deleteItem(int key);
+    void deleteItem(int key)
+    {
+        tNode * foundNode = findNode(root,key);
+	cout << "return val of findNode: " << foundNode << endl;
+	if(foundNode == nullptr)
+	{
+	    fprintf(stderr, "node not found with key %d\n", key);
+	    exit(-1);
+	}
+	// node has one child
+	if( (foundNode->right == nullptr && foundNode->left != nullptr)
+	|| ( foundNode->right != nullptr && foundNode->left == nullptr))
+	    deleteSingleChildNode(foundNode);
+	// node has two children
+	else if(foundNode->right != nullptr && foundNode->left != nullptr)
+	    deleteDoubleChildNode(foundNode);
+	// node has no children
+	else
+	    deleteNoChildNode(foundNode);
+    }
     void print()
     {
 	if(isEmpty())
@@ -49,21 +72,26 @@ class BinaryTree
 	{
 	    int level = 0;
 	    visual_rep(root, level);
-	    inOrderTrav(root);
 	}
     }
   private:
     tNode * root;
+    int numNodes;
     tNode * findNode(tNode * tRoot, int key)
     {
+	if(tRoot != nullptr)
+	    cout << "comp key " << key << " with node key " << tRoot->key << endl;
 	if(tRoot == nullptr)
 	    return nullptr;
         if (tRoot->key == key)
+	{
+	    cout << key << " == " << tRoot->key << " key found" << endl;
 	    return tRoot;
+	}
 	else if(key > tRoot->key)
-	    findNode(tRoot->right, key);
+	    return findNode(tRoot->right, key);
 	else if(key < tRoot->key)
-	    findNode(tRoot->left, key);
+	    return findNode(tRoot->left, key);
 	return nullptr;
     }
     void visual_rep(tNode * tRoot, int level)
@@ -72,8 +100,7 @@ class BinaryTree
 	{
 	    visual_rep(tRoot->right, ++level);
 	    cout << setw(10 * level) << "key: " << tRoot->key << " val: " << tRoot->val << endl;
-	    level = 0;
-	    visual_rep(tRoot->left, ++level);
+	    visual_rep(tRoot->left, level++);
 	}
     }
     void inOrderTrav(tNode * tempRoot)
@@ -89,9 +116,21 @@ class BinaryTree
     {
         if(tempRoot == nullptr)
 	    tempRoot = newNode;
-        else if ( newNode->key > tempRoot->key )
-	    insert(tempRoot->right, newNode);
+        else if ( newNode->key < tempRoot->key )
+	    insert(tempRoot->left, newNode);
         else
-            insert(tempRoot->left, newNode);	   
+            insert(tempRoot->right, newNode);	   
+    }
+    void deleteSingleChildNode(tNode * & node)
+    {
+        perror("deleteSingleChildNode not implemented\n");
+    }
+    void deleteDoubleChildNode(tNode * & node)
+    {
+        perror("deleteDoubleChildNode not implemented\n");
+    }
+    void deleteNoChildNode(tNode* & node)
+    {
+        perror("deleteNoChildNode not implemented\n");
     }
 };
--- main.cpp
+++ main.cpp
@@ -11,28 +11,57 @@ void test_constr()
 void test_insert()
 {
     BinaryTree myTree;
-    myTree.putItem(2,"two");
-    myTree.putItem(1,"one");
-    myTree.putItem(3,"three");
+    myTree.putItem(50,"test");
+    myTree.putItem(75,"test");
+    myTree.putItem(25,"test");
+    myTree.putItem(85,"test");
+    myTree.putItem(65,"test");
+    myTree.putItem(15,"test");
+    myTree.putItem(35,"test");
     cout << "test print\n";
     myTree.print();
 }
 
 void test_getItem()
 {
-    string result = "";
     BinaryTree myTree;
     myTree.putItem(2,"two");
     myTree.putItem(1,"one");
     myTree.putItem(3,"three");
-    cout << "calling getItem on 2 (present) result: " << myTree.getItem(2) << endl;
+    for(int i = 0; i < 3; ++i)
+        cout << "calling getItem on " << i <<  " (present) result: " << myTree.getItem(i) << endl;
     cout << "calling getItem on 4 (not present) result: " << myTree.getItem(4) << endl;
+}
+
+void test_getLength()
+{
+    BinaryTree myTree;
+    myTree.putItem(2,"two");
+    myTree.putItem(1,"one");
+    myTree.putItem(3,"three");
+    myTree.putItem(4,"four");
+    myTree.putItem(5,"five");
+    cout << "test of getLength() on tree with 5 elements: " << myTree.getLength() << endl;
+}
+void test_deleteItem()
+{
+    BinaryTree myTree;
+    myTree.putItem(2,"two");
+    myTree.putItem(1,"one");
+    myTree.putItem(3,"three");
+    myTree.putItem(4,"four");
+    myTree.putItem(0,"zero");
+    myTree.print();
+    for(int i = 2; i < 5; ++i)
+        myTree.deleteItem(i);
 }
 
 int main()
 {
 //    test_constr();
 //    test_insert();
-    test_getItem();
+//    test_getItem();
+//    test_getLength();
+    test_deleteItem();
     return 0;
 }
