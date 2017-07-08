de778caa2bda5d32fc73a24a09449279ac1dadb6
Sean <seanlcarstensen@gmail.com>
2017-07-07 18:42:48 -0700

    getItem implemented and class reorganization

--- BinaryTree.h
+++ BinaryTree.h
@@ -30,32 +30,17 @@ class BinaryTree
     }
     bool isFull();
     int getLength();
-    int getItem(int key);
+    string getItem(int key)
+    {
+	tNode * temp = findNode(root,key);
+	return (temp == nullptr) ? "not found" : temp->val;
+    }
     void putItem(int key, string val)
     {
 	tNode * newNode = new tNode(key,val);
 	insert(root,newNode);
     }
-    void insert(tNode * & tempRoot, tNode * newNode)
-    {
-        if(tempRoot == nullptr)
-	    tempRoot = newNode;
-        else if ( newNode->key > tempRoot->key )
-	    insert(tempRoot->right, newNode);
-        else
-            insert(tempRoot->left, newNode);	   
-    }
     void deleteItem(int key);
-    void visual_rep(tNode * tRoot, int level)
-    {
-	if(tRoot != nullptr)
-	{
-	    visual_rep(tRoot->right, ++level);
-	    cout << setw(10 * level) << "key: " << tRoot->key << " val: " << tRoot->val << endl;
-	    level = 0;
-	    visual_rep(tRoot->left, ++level);
-	}
-    }
     void print()
     {
 	if(isEmpty())
@@ -67,6 +52,30 @@ class BinaryTree
 	    inOrderTrav(root);
 	}
     }
+  private:
+    tNode * root;
+    tNode * findNode(tNode * tRoot, int key)
+    {
+	if(tRoot == nullptr)
+	    return nullptr;
+        if (tRoot->key == key)
+	    return tRoot;
+	else if(key > tRoot->key)
+	    findNode(tRoot->right, key);
+	else if(key < tRoot->key)
+	    findNode(tRoot->left, key);
+	return nullptr;
+    }
+    void visual_rep(tNode * tRoot, int level)
+    {
+	if(tRoot != nullptr)
+	{
+	    visual_rep(tRoot->right, ++level);
+	    cout << setw(10 * level) << "key: " << tRoot->key << " val: " << tRoot->val << endl;
+	    level = 0;
+	    visual_rep(tRoot->left, ++level);
+	}
+    }
     void inOrderTrav(tNode * tempRoot)
     {
         if( tempRoot != nullptr)
@@ -76,6 +85,13 @@ class BinaryTree
 	    inOrderTrav(tempRoot->right);
 	}
     }
-  private:
-    tNode * root;
+    void insert(tNode * & tempRoot, tNode * newNode)
+    {
+        if(tempRoot == nullptr)
+	    tempRoot = newNode;
+        else if ( newNode->key > tempRoot->key )
+	    insert(tempRoot->right, newNode);
+        else
+            insert(tempRoot->left, newNode);	   
+    }
 };
--- main.cpp
+++ main.cpp
@@ -18,9 +18,21 @@ void test_insert()
     myTree.print();
 }
 
+void test_getItem()
+{
+    string result = "";
+    BinaryTree myTree;
+    myTree.putItem(2,"two");
+    myTree.putItem(1,"one");
+    myTree.putItem(3,"three");
+    cout << "calling getItem on 2 (present) result: " << myTree.getItem(2) << endl;
+    cout << "calling getItem on 4 (not present) result: " << myTree.getItem(4) << endl;
+}
+
 int main()
 {
 //    test_constr();
-    test_insert();
+//    test_insert();
+    test_getItem();
     return 0;
 }
